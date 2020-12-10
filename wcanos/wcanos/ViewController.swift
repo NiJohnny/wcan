4042c833349863559be2b159a527db6cafc6d63b
yumingming <mingmingyush@wecanintl.com>
2020-12-10 13:17:01 +0800

    wcan项目初始化

---
+++ wcanos/wcanos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wcanos
+//
+//  Created by erp on 2020/12/10.
+//  Copyright © 2020 erp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
